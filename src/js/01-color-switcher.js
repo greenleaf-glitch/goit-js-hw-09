b32df15da19bc91af9df4aa25ae4fcc76992b21c
greenleaf-glitch <133708156+greenleaf-glitch@users.noreply.github.com>
2023-08-16 22:05:04 +0300

    upd task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
-// ddffdg
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  bodyEl: document.body,
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+let changeColorIntervalID;
+
+function onStartBtnClick() {
+  changeColorIntervalID = setInterval(() => {
+    const currentBgColor = getRandomHexColor();
+    refs.bodyEl.style.backgroundColor = currentBgColor;
+    refs.startBtn.setAttribute('disabled', 'disabled');
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  clearInterval(changeColorIntervalID);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
