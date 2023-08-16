11cfa0803a73a5945830226e7d351300240ed1ec
greenleaf-glitch <133708156+greenleaf-glitch@users.noreply.github.com>
2023-08-16 19:05:31 +0000

    Deploying to gh-pages from @ greenleaf-glitch/goit-js-hw-09@b32df15da19bc91af9df4aa25ae4fcc76992b21c 🚀

---
+++ 01-color-switcher.110a3560.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),bodyEl:document.body};let e;t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.bodyEl.style.backgroundColor=e,t.startBtn.setAttribute("disabled","disabled")}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.110a3560.js.map
---
+++ 01-color-switcher.7d795bef.js
@@ -0,0 +1,2 @@
+!function(){var t,n={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),bodyEl:document.body};n.startBtn.addEventListener("click",(function(){t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));n.bodyEl.style.backgroundColor=t,n.startBtn.setAttribute("disabled","disabled")}),1e3)})),n.stopBtn.addEventListener("click",(function(){clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.7d795bef.js.map
