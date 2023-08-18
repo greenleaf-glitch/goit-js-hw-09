a7fbb9073a7dded3d8228f5a371bdce8256243b5
greenleaf-glitch <133708156+greenleaf-glitch@users.noreply.github.com>
2023-08-18 19:30:55 +0000

    Deploying to gh-pages from @ greenleaf-glitch/goit-js-hw-09@4019703d9ceb87e7277643d1fd677a40621b2811 🚀

---
+++ 01-color-switcher.2de84762.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),bodyEl:document.body};let e;t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.bodyEl.style.backgroundColor=e,t.startBtn.disabled=!0}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.2de84762.js.map
